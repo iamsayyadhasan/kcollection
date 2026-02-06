9c35f239b1d33f59ec13ae3db105d071baa15a88
iamsayyadhasan <hasanshahzad82@gmail.com>
2026-02-06 23:04:29 +0530

    commit message

--- src/components/for-women/WBanner.js
+++ src/components/for-women/WBanner.js
@@ -1,30 +1,24 @@
 "use client";
+import Image from "next/image";
 
 export default function Wbanner() {
   return (
     <section
       aria-label="Women Banner"
-      className="relative w-full h-[65vh] md:h-[80vh] lg:h-[80vh] overflow-hidden"
+      className="relative w-full h-[65vh] md:h-[80vh] overflow-hidden"
     >
-      {/* Image */}
-      <picture>
-        <source media="(max-width: 767px)" srcSet="/images/Charizma-1.jpeg" />
-        <source
-          media="(min-width: 768px)"
-          srcSet="/images/hero-desktop-1.jpg"
-        />
-        <img
-          src="/images/hero-desktop-1.jpg"
-          alt="For Women"
-          className="w-full h-full object-cover"
-        />
-      </picture>
+      <Image
+        src="/images/hero-desktop-1.jpg"
+        alt="For Women"
+        fill
+        priority   // 🔥 LCP FIX
+        sizes="100vw"
+        className="object-cover"
+      />
 
-      {/* Dark Overlay */}
       <div className="absolute inset-0 bg-black/40"></div>
 
-      {/* Text Bottom Left */}
-      <div className="absolute bottom-8 left-6 md:bottom-12 md:left-12 lg:left-20 text-white">
+      <div className="absolute bottom-8 left-6 md:bottom-12 md:left-12 text-white">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
           For Women
         </h1>
