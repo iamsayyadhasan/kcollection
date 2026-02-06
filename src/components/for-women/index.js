84b05d093f9c59bb63e2f3e9def485248ff80d89
iamsayyadhasan <hasanshahzad82@gmail.com>
2026-02-06 23:20:10 +0530

    commit message

--- src/components/for-women/index.js
+++ src/components/for-women/index.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import Wbanner from './WBanner'
-import WomenSuitsSection from './Category'
-import Socialmedia from '../Home/Socialmedia'
+import Wbanner from "./WBanner";
+import WomenSuitsSection from "./WomenSuitsSection.client";
+import Socialmedia from "../Home/Socialmedia";
 
-function Womenpage() {
-  return (
-    <div>
-      <Wbanner/>
-      <WomenSuitsSection/>
-      <Socialmedia/>
-    </div>
-  )
+// 🔥 Server-side fetch (fixes LCP/FCP on Vercel)
+async function getWomenProducts() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/product/get-all-products`,
+    {
+      next: { revalidate: 3600 }, // ✅ CDN caching
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  const data = await res.json();
+
+  return (data.products || []).filter(
+    (item) => item.category === "Women"
+  );
 }
 
-export default Womenpage
+// ✅ SERVER COMPONENT
+export default async function Womenpage() {
+  const products = await getWomenProducts();
+
+  return (
+    <div>
+      {/* Static + priority image (LCP-safe) */}
+      <Wbanner />
+
+      {/* ONLY dynamic section */}
+      <WomenSuitsSection products={products} />
+
+      {/* Static section (SSR is good here) */}
+      <Socialmedia />
+    </div>
+  );
+}
